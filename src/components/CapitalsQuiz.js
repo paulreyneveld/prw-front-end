34fac3b620ad82213f1077f60b1885d0367142fe
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-12-09 22:03:36 -0800

    Trying to establish uniqueness of countryList with Set type

--- src/components/CapitalsQuiz.js
+++ src/components/CapitalsQuiz.js
@@ -1,5 +1,19 @@
 import React from 'react'
+// First, I need to import the compiled json for all of the countries/capitals.
 import countryInfo from '../countryInfo.json'
+
+/* Okay, so what are the steps I need to take?
+
+First, I need to import the compiled json for all of the countries/capitals.
+
+Second, I need to generate a random country/capital combination. One of these will be the question answer.
+
+Third, I need to generate three more random country/capital combinations. I don't want there to be any
+duplicates. 
+
+*/ 
+
+// Second, I need to generate a random country/capital combination. One of these will be the question answer.
 
 const CapitalsQuiz = () => {
 
@@ -14,23 +28,22 @@ const CapitalsQuiz = () => {
         for (let i = 0; i < 4; i++) {
             countryList.push(getRandomCountry())
         }
-        return countryList
+        let countrySet = new Set(countryList)
+        if (countrySet.length < 4) {
+            makeRandomList()
+        }
+        return countrySet
     }
 
-    let answer = getRandomCountry()
+    let randomList = makeRandomList()
+    console.log(randomList)
+    let arrayified = new Array(randomList)
+    console.log(arrayified)
 
     return (
         <>
         <h1>Testing</h1>
-        <p>What is the capital of {answer.country}</p>
-        <button>{answer.city}</button>
-        <br></br>
-        <button>{getRandomCountry().city}</button>
-        <button>{getRandomCountry().city}</button>
-        <button>{getRandomCountry().city}</button>
-
-
-
+        <p>What is the capital of</p>
         </>
     )
 }
