0516b3c6b4a49570791f1ba7142d30fe2311b180
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-12-20 19:21:20 -0800

    Added support for Merriam-Webster API

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -4,12 +4,21 @@ import axios from 'axios'
 
 const Dictionary = () => {
 
+    // Merriam API KEY
+    const API_KEY = `9e686b93-b427-4a2d-9f53-14c1ea4f8f8a`
+    const API_URL = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=${API_KEY}`
+
     const [searchTerm, setSearchTerm] = useState('')
+
     
     const queryAPI = (event) => {
         event.preventDefault()
         console.log(searchTerm)
+        axios.get(API_URL)
+            .then(response => console.log(response.data))
+
     }
+
     return (
         <>
         <h1> hello world</h1> 
