627838185956d692f933247b0b4e78eb57263875
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-12-20 18:56:29 -0800

    Added search field

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
+
 const Dictionary = () => {
+
+    const [searchTerm, setSearchTerm] = useState('')
+    
+    const queryAPI = (event) => {
+        event.preventDefault()
+        console.log(searchTerm)
+    }
     return (
+        <>
         <h1> hello world</h1> 
+        <form onSubmit={queryAPI}>
+            <label>Search Term:</label>
+            <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
+            <button>Submit</button>
+        </form>
+        </>
     )
 }
 
