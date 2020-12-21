9a2d85445253e5654f2e17ce087bfeeef89ce251
Paul <63985305+paulreyneveld@users.noreply.github.com>
2020-12-20 19:30:24 -0800

    Removed sensitive information

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -5,19 +5,26 @@ import axios from 'axios'
 const Dictionary = () => {
 
     // Merriam API KEY
-    const API_KEY = `9e686b93-b427-4a2d-9f53-14c1ea4f8f8a`
+    const API_KEY = ``
     const API_URL = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=${API_KEY}`
 
     const [searchTerm, setSearchTerm] = useState('')
-
+    const [definition, setDefinition] = useState([])
     
     const queryAPI = (event) => {
         event.preventDefault()
         console.log(searchTerm)
         axios.get(API_URL)
-            .then(response => console.log(response.data))
+            .then(response => {
+                console.log(response.data)
+                setDefinition(response.data[0].shortdef)
+            })
+            .catch(error => console.log(error))
 
     }
+
+    console.log(definition)
+
 
     return (
         <>
@@ -27,6 +34,7 @@ const Dictionary = () => {
             <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
             <button>Submit</button>
         </form>
+        {definition.map(entry => <p>{entry}</p>)}
         </>
     )
 }
