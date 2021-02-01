63e9d80babf4b7e22f185648fa9bc9f6a225b031
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-31 21:20:13 -0800

    Wired up front and back end to allow for blog posts retrieved from the api

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const BlogPosts = () => {
+
+    const [blogPosts, setBlogPosts] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/blogs')
+            .then(response => {
+                console.log(response)
+                setBlogPosts(response)
+            })
+            .catch(error => console.log(error))
+    }, [])
+
     return (
         <h1>Testing</h1>
     )
