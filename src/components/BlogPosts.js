fa2aef4afd87b4839dd6cb9468027d57d002b295
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-01-31 21:29:41 -0800

    Finished up BlogPosts component with dummy data from backend

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -8,14 +8,22 @@ const BlogPosts = () => {
     useEffect(() => {
         axios.get('http://localhost:3001/blogs')
             .then(response => {
-                console.log(response)
-                setBlogPosts(response)
+                console.log(response.data)
+                setBlogPosts(response.data)
             })
             .catch(error => console.log(error))
     }, [])
 
     return (
+        <>
         <h1>Testing</h1>
+        {blogPosts.map(post => 
+        <div key={post._id}>
+        <h3>{post.title}</h3>
+        <p>{post.content}</p>
+        </div>
+        )}
+        </>
     )
 }
 
