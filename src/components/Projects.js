9539728520c7c5161f5244d061e3d4323bc40513
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-02 16:12:41 -0800

    Gallery import a meaningful variable name

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import { Col, Row, Image } from 'react-bootstrap'
-import test from '../images/grocery-app-screen.png'
+import groceryImage from '../images/grocery-app-screen.png'
 
 const Projects = () => {
     return (
         <Row>
         <Col className="gutterfix">
-                <a href="https://immense-lowlands-07888.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Image src={test} /></a>
+                <a href="https://immense-lowlands-07888.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Image src={groceryImage} /></a>
         </Col>
         <Col>
             <a href="https://immense-lowlands-07888.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Image src={test} /></a>
         </Col>
-        <Col>
-            <a href="https://immense-lowlands-07888.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Image src={test} /></a>
-        </Col>
+
         </Row>
         
     )
