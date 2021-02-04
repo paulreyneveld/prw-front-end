b3c9ed9e7d6bb7d164d49cdf465f00bc19895782
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-02-03 18:58:01 -0800

    Resized and replaced screen

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Col, Row, Image } from 'react-bootstrap'
 import groceryImage from '../images/grocery-app-screen.png'
 import quizAppImage from '../images/quiz-app-screen.png'
+import coursesAppImage from '../images/courses-app-screen.png'
 
 const Projects = () => {
     return (
         <Row>
         <Col className="gutterfix">
-                <a href="https://immense-lowlands-07888.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Image src={groceryImage} /></a>
+                <a href="https://thproj10api.herokuapp.com/courses" target="_blank" rel="noopener noreferrer"><Image src={coursesAppImage} /></a>
+        </Col>
+        <Col>
+        <a href="https://immense-lowlands-07888.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Image src={groceryImage} /></a>
         </Col>
         <Col>
             <a href="https://paulreyneveld.github.io/THProj4/" target="_blank" rel="noopener noreferrer"><Image src={quizAppImage} /></a>
